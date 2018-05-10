d204623097546a39dd6f660173ce0d8ada9e4bfa
Emerson Santana <emersonfidelon@gmail.com>
2018-05-10 01:22:02 -0300

    Autenticação usando jwt e hash

--- src/controllers/projectController.js
+++ src/controllers/projectController.js
@@ -6,7 +6,7 @@ const router = express.Router();
 router.use(authMiddleware);
 
 router.get('/', (req, res) => {
-    res.send({ok:true, userId: req.userId});
+    res.send({logged:true, userId: req.userId});
 });
 
 module.exports = app => app.use('/projects', router);
